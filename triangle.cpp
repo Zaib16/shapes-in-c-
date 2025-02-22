558acdedbf2c84a339165e6de47264a422f3c2cc
Zaib16 <zaibgull1423@gmail.com>
2025-02-22 17:08:17 +0500

    first push

---
+++ triangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main(){
+system("Color CB");
+cout<<"               ."<<endl;
+cout<<"              . ."<<endl;
+cout<<"   	    .   ."<<endl;
+cout<<"            .     ."<<endl;
+cout<<"           .       ."<<endl;
+cout<<"     	 .         ."<<endl;
+cout<<"		.           ."<<endl;
+cout<<"        .             ."<<endl;
+cout<<"       .               ."<<endl;
+cout<<"      .   	       ."<<endl;
+cout<<"     .    		."<<endl;
+cout<<"    . 			 ."<<endl;
+cout<<"   .			  ."<<endl;
+cout<<"  .			   ."<<endl;
+cout<<" .			    ."<<endl;
+cout<<"..............................."<<endl;
+}
